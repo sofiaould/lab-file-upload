09a22465b5db3a75c215eae7ca6546a06b828446
Adam Ghaoul <adamghaoul@gmail.com>
2021-05-22 16:50:45 +0200

    1st iteration done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,6 +8,8 @@ const User = require('../models/User.model');
 const mongoose = require('mongoose');
 
 const routeGuard = require('../configs/route-guard.config');
+const fileUploader = require('../configs/cloudinary.config')
+
 
 ////////////////////////////////////////////////////////////////////////
 ///////////////////////////// SIGNUP //////////////////////////////////
@@ -17,9 +19,9 @@ const routeGuard = require('../configs/route-guard.config');
 router.get('/signup', (req, res) => res.render('auth/signup'));
 
 // .post() route ==> to process form data
-router.post('/signup', (req, res, next) => {
+router.post('/signup', fileUploader.single('image'), (req, res, next) => {
   const { username, email, password } = req.body;
-
+  const imageUrl = req.file.path;
   if (!username || !email || !password) {
     res.render('auth/signup', { errorMessage: 'All fields are mandatory. Please provide your username, email and password.' });
     return;
@@ -45,7 +47,8 @@ router.post('/signup', (req, res, next) => {
         // passwordHash => this is the key from the User model
         //     ^
         //     |            |--> this is placeholder (how we named returning value from the previous method (.hash()))
-        passwordHash: hashedPassword
+        passwordHash: hashedPassword,
+        imageUrl: imageUrl
       });
     })
     .then(userFromDB => {
